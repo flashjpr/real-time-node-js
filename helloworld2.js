ca0e0438850b3ed031ac11c95d9777910d53a51e
flashjpr <flashjpr@gmail.com>
2019-05-08 21:58:39 +0100

    IO modules using asynquence

---
+++ helloworld2.js
@@ -0,0 +1,25 @@
+
+function readFile(filename) {
+    var sq = ASQ();
+
+    fs.readFile(filename, sq.errfcb());
+
+    return sq;
+}
+
+function delayMsg(done, contents) {
+    setTimeout(function () {
+       done(contents);
+    }, 1000)
+}
+
+function say(filename) {
+    return readFile(filename)
+        .then(delayMsg)
+}
+
+let fs = require('fs');
+let ASQ = require('asynquence');
+require('asynquence-contrib');
+
+module.exports.say = say;
