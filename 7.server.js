dd8c3d3c048c35c9bb776b6a0a81ebb01cbbf262
flashjpr <flashjpr@gmail.com>
2019-05-09 00:05:59 +0100

    Setting up Socket.io

---
+++ 7.server.js
@@ -0,0 +1,33 @@
+function handleHTTP(req, res) {
+
+    if (req.method === 'GET') {
+        if (/^\/\d+(?=$|[\/?#])/.test(req.url)) {
+            req.addListener("end",function(){
+                req.url = req.url.replace(/^\/(\d+).*$/,"/$1.html");
+                static_files.serve(req,res);
+            });
+            req.resume();
+        } else {
+            res.writeHead(403);
+            res.end('Go away');
+        }
+    } else {
+        res.writeHead(403);
+        res.end('Go away');
+    }
+
+}
+
+var http = require('http');
+var http_server = http.createServer(handleHTTP);
+var host = 'localhost';
+var port = 8006;
+
+var node_static = require('node-static');
+var static_files = new node_static.Server(__dirname);
+
+var io = require('socket.io');
+
+io.listen(http_server);
+
+http_server.listen(port, host);
